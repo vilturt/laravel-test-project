909d0bf621119d317ba22bff0fb690cef89b5e56
vilturt <viljamiturtiainen@gmail.com>
2019-05-26 10:32:22 +0300

    Created model, migration file and controller and runned migrate to create and update used database tables.

--- app/Document.php
+++ app/Document.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    public function user() {
+
+        return $this->hasOne('App\User');
+
+    }
 }
--- database/migrations/2019_05_26_062339_create_documents_table.php
+++ database/migrations/2019_05_26_062339_create_documents_table.php
@@ -13,11 +13,24 @@ class CreateDocumentsTable extends Migration
      */
     public function up()
     {
+
+        // creating columns for documents table
+
         Schema::create('documents', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->increments('id'); // primary key of this table
+
+            // let's add some more table contents
+
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('title', 100);
+            // lenght of title must be at least 5,remember to check it
+            $table->timestamps(); // adds created_at and updated_at
+            // we need also deleted_at so we are adding it
+            $table->softDeletes(); // deleted_at
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
+
 
     /**
      * Reverse the migrations.
